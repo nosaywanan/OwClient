4245d033d85fe8f5993608b62809d7ff6dd5b7a8
nosaywanan <100969567@qq.com>
2016-09-12 15:26:36 +0800

    change tab title

--- app/src/main/java/com/example/administrator/overwatch/MainActivity.java
+++ app/src/main/java/com/example/administrator/overwatch/MainActivity.java
@@ -118,9 +118,9 @@ public class MainActivity extends AppCompatActivity implements NewsItemBiz.OnPic
             public CharSequence getPageTitle(int position) {
                 switch (position) {
                     case 0:
-                        return "新闻1";
+                        return "新闻";
                     case 1:
-                        return "攻略1";
+                        return "攻略";
                     default:
                         return null;
                 }
